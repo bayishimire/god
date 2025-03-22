df31bce7cdb443fd7ed68937e73349f70462036d
bayishimire <bayishimiresamuel760@gmail.com>
2025-03-22 18:50:08 +0200

    python check add number bwetwen range

--- bayii.py
+++ bayii.py
@@ -1,1 +1,18 @@
-print(" sam is ....")
+# x=int(input("entermark :" ))
+# if marks>=70:
+#     grade="firstclass"
+#     print(grade)
+# elif marks>=65 and marks<70:
+#     grade="second up"
+#     print(grade)
+# else:
+#     priny=("pass")
+x=int(input("enterx "))
+n=int(input("entern "))
+for x in range(n):
+ if x%2 !=0:
+   print(x)
+   if x==16:
+    break
+
+
