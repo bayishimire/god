ef835af1da43cdc7d349af15fc47599bf403e898
bayishimire <bayishimiresamuel760@gmail.com>
2025-03-29 15:34:18 +0000

    hou use --init-- in oop

---
+++ oop.py
@@ -0,0 +1,19 @@
+class CST:
+    def __init__(self,names,number):
+        self.names=names
+        self.number=number
+IT=CST("well cam samuelcst welcome our classs","2023_2025")
+
+print(IT.names)
+print(IT.number)
+# class cst:
+#     def school(self,name,year,id):
+#         self.name=name
+#         self.id=id
+#         self.year=year
+
+# it=cst()
+# it.school("well cam samuelcst welcome our classs",120048007245022,"20203_2025")
+# print(it.name)
+# print(it.id)
+# print(it.year)
